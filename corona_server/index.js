bb40e291affd16e8cdba4f94f157187eab5bacbb
St0ffel <melsteffen2000@gmail.com>
2020-10-26 15:39:17 +0100

    Refactoring Testing Files

--- corona_server/index.js
+++ corona_server/index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express();
-const port = 8000;
+/**
+ * @file Run this file on a server to start the Corona-Timeslot application application
+ * @version 0.0.1
+ */
 
-app.get('/', (req, res) => {
+const f_express = require('express')
+
+// TODO: change port assignment when server gets deployed to production
+// TODO: maybe also rework local log, if needed create log modules to make it easier to change later on
+const n_port = 8000;
+
+const o_app = f_express();
+
+
+
+
+// Initialize API-Router
+const o_apiRouter = require('./modules/backend/apiRouter')
+o_app.use('/api', o_apiRouter)
+
+// TODO: Remove before going production
+// simple test to see if server is accessible
+o_app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+// local log that server ist listening
+o_app.listen(n_port, () => {
+  console.log(`Example app listening on port ${n_port}!`)
 });
+
+
+
+
+
---
+++ corona_server/modules/backend/test.js
@@ -0,0 +1,69 @@
+// TODO: Delete this file before going production
+
+/**
+ * @module test
+ * @version 0.0.1
+ */
+
+/**
+ * Handler for express request.
+ * 
+ * Returns test data for an given id. If Id is not valid or an internal server errror occures,
+ * the response object is changed correspondingly.
+ * 
+ * @param {Express.Request} req A request based on the Express framework
+ * @param {Express.Response} res A Response based on the express framework, when the Promises resolves, this is sent to the client
+ */
+function f_requestHandler(req, res, next) {
+    f_getTestData(req.params.testId)
+        .then(
+            (fulfilled) => {
+                res.status(200);
+                res.json(fulfilled);
+            },
+            (rejected) => {
+                res.status(rejected.status);
+                res.json(rejected.errorData);
+            }
+        ).catch(
+            (err) => {
+                next(err);
+        });
+}
+/**
+ * Get test data.
+ * 
+ * Takes an String and returns an corresponding json
+ * 
+ * @param {String} testId Eine Id die aus der Request entnommen wird
+ * @returns {Promise} Gibt ein Promise zurück dass mit ein JSON resolved oder rejected
+ */
+
+function f_getTestData(testId) {
+    return new Promise((resolve, reject) => {
+        if (testId == 1) {
+            resolve({
+                id: "stuff",
+                fName: "11111",
+                lName: "11111"
+            });
+        }
+        else if (testId == 2) {
+            resolve({
+                id: "stuff2",
+                fName: "22222",
+                lName: "22222"
+            });
+        }
+        else {
+            reject({
+                status : 404,
+                errorData : {
+                    message : "No data found for id: " + testId
+                }
+            });
+        }
+    });
+}
+
+module.exports.requestHandler = f_requestHandler
